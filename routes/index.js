9b65b53b1fb808fc158d98a9cdf00945d68bc8e9
Liucw <279674457@qq.com>
2013-10-03 14:31:12 +0800

    add

--- routes/index.js
+++ routes/index.js
@@ -157,9 +157,10 @@ var s = setInterval(function(){
 }, 1000);
 
 var cleanRoom = function(rooms){
-    console.log('====cleanRoom time:'+new Date().getTime());
     for(var i in rooms){
         if(newDate().getTime() - rooms[i].update > 1*60*1000)
-            rooms.splice(i, 1);
+            console.log('====cleanRoom time:'+new Date().getTime());
+            console.log('====cleanRoom :'+i);
+            delete rooms[i];
     }
 }
