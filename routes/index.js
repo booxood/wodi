9baaafc7fe552f2f7e954a0120374e9eb8484211
Liucw <279674457@qq.com>
2013-10-04 22:57:46 +0800

    add

--- routes/index.js
+++ routes/index.js
@@ -68,7 +68,7 @@ exports.post = function(req, res){
     }
     console.log('-----------------  rooms   --------------------');
     for(var i in rooms){
-        console.log(i+'------'+rooms[i].update+'-----'+rooms[i].players.length);
+        console.log(i+'------'+rooms[i].host+'-----'+rooms[i].players.length);
     }
     console.log('------------------------------------------------------');
     console.log('------------------------------------------------------');
