ed12f34395631224543a239b805b04bdfd0a5be8
Liucw <279674457@qq.com>
2013-10-04 21:53:37 +0800

    add

--- models/room.js
+++ models/room.js
@@ -2,8 +2,8 @@ var Room = function(id, host){
 	this.id = id;
 	this.host = host;
 
-	this.playerNum = 6;
-	this.spyNum = 1;
+	this.playerNum = 0;
+	this.spyNum = 0;
 	this.nullNum = 0;
 
 	this.players = [];
@@ -16,6 +16,18 @@ Room.prototype.addPlayer = function(player){
 	this.update = new Date().getTime();
 
 	// Event.emitor('add')
+}
+
+Room.prototype.valid = function(){
+	if(this.playerNum <= 2){
+		return '玩家数 太少';
+	}else if(this.spyNum > 0 && this.nullNum >= 0){
+		return '卧底数 和 白板数 太少';
+	}else if(this.playerNum <= this.spyNum + this.nullNum){
+		return '都是卧底和白板怎么玩？';
+	}
+
+	return '';
 }
 
 module.exports = Room;
--- routes/index.js
+++ routes/index.js
@@ -130,12 +130,17 @@ exports.post = function(req, res){
                 if(isNum(cmd[1]) && isNum(cmd[2]) && isNum(cmd[3])){
                     var id = randomNum(4);
                     var room = new Room(id, msg.FromUserName);
-                    room.playerNum = cmd[1];
-                    room.spyNum = cmd[2];
-                    room.nullNum = cmd[3];
+                    room.playerNum = parseInt(cmd[1]);
+                    room.spyNum = parseInt(cmd[2]);
+                    room.nullNum = parseInt(cmd[3]);
+
+                    if(room.valid()){
+                        return room.valid();
+                    }
+
                     rooms[id] = room;
-                    resStr = '房间创建成功，ID:'+id+';让小伙伴们发送：wodi 房间ID 昵称;例如：\n'+
-                            'wodi '+id+' 小白';
+                    resStr = '房间创建成功，ID:'+id+';让小伙伴们发送：wodiroom 房间ID 昵称;例如：\n'+
+                            'wodiroom '+id+' 小白';
                 }else{
                     resStr = '玩家数 卧底人数 白板人数 必须是数字！';
                 }
@@ -154,14 +159,12 @@ exports.post = function(req, res){
 
 var s = setInterval(function(){
     cleanRoom(rooms);
-}, 1000);
+}, 100000);
 
 var cleanRoom = function(rooms){
     for(var i in rooms){
-        // console.log('========================================');
-        // console.log(new Date().getTime());
         console.log(rooms[i].update);
-        if(new Date().getTime() - rooms[i].update > 1*60*1000)
+        if(new Date().getTime() - rooms[i].update > 30*60*1000)
         {
             console.log('==========cleanRoom time:'+new Date().getTime());
             console.log('==========cleanRoom :'+i);
